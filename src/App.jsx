32b42e7fd5bc6c0d7e00b3806699c4a213b2ef9f
valyukhov <alexzed@bk.ru>
2023-08-15 19:07:21 +0300

    save last file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import LanguageSelect from "./components/LanguageSelect";
 import Header from "./components/Header";
 import VerseSlider from "./components/VerseSlider";
 
-import { useTranslation } from "react-i18next";
 import About from "./components/About";
 
 function App() {
