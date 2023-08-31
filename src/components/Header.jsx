0dfe865f6f6f47649e7cc9a5d702c6c4dd36c64a
DenisArger <den.arger@gmail.com>
2023-08-31 13:22:38 +0300

    feat: add switch dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,44 @@
+import  { useState, useEffect } from 'react';
+import { FiSun, FiMoon } from 'react-icons/fi';
 import LocaleSwitch from './LocaleSwitch';
 
 function Header() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    const newMode = !isDarkMode;
+    setIsDarkMode(newMode);
+    localStorage.setItem('darkMode', newMode.toString()); 
+  };
+
+  useEffect(() => {
+    const savedDarkMode = localStorage.getItem('darkMode');
+    if (savedDarkMode) {
+      setIsDarkMode(savedDarkMode === 'true');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
   return (
-    <div className="flex flex-row py-6 justify-center relative">
-      <div className="font-['Anek_Bangla'] font-bold uppercase text-2xl">
+    <div className={`flex flex-row py-6 justify-between items-center relative`}>
+      <div className="mx-auto ml-auto font-['Anek_Bangla'] font-bold uppercase text-2xl">
         3:16 John
       </div>
-      <div className="absolute right-0">
+      <div className="flex items-center">
+        <button
+          className="text-3xl ml-4"
+          onClick={toggleDarkMode}
+          aria-label="Toggle Dark Mode"
+        >
+          {isDarkMode ? <FiSun /> : <FiMoon />}
+        </button>
         <LocaleSwitch />
       </div>
     </div>
