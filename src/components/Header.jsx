415690a8fdc0e292d38c99666393520878cf1f23
Denis Arger <den.arger@gmail.com>
2023-09-06 13:04:17 +0300

    fix: delete extract div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,7 @@ import DarkModeToggle from "./DarkModeToggle";
 function Header() {
   return (
     <div className="flex flex-row py-6 justify-between items-center relative">
-      <div className="flex-grow font-['Anek_Bangla'] font-bold uppercase text-2xl ml-28 md:hidden">
-        3:16 John
-      </div>
-      <div className="flex-grow font-['Anek_Bangla'] font-bold uppercase text-2xl ml-96 hidden md:flex">
+      <div className="flex-grow font-['Anek_Bangla'] font-bold uppercase text-2xl ml-28 md:ml-96">
         3:16 John
       </div>
       <DarkModeToggle />
