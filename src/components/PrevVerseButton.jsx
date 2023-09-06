11ec226cddfbab9635de7e93a3b2fe60d6238fb9
Denis Arger <den.arger@gmail.com>
2023-09-06 11:58:35 +0300

    fix: delete proTypes  PrevVerseButton

--- src/components/PrevVerseButton.jsx
+++ src/components/PrevVerseButton.jsx
@@ -1,16 +1,18 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const PrevVerseButton = ({ onClick }) => (
   <button
     className="bg-zinc-100 dark:bg-zinc-800 rounded-full p-3"
-    onClick={onClick}>
+    onClick={onClick}
+  >
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth="1.5"
       stroke="currentColor"
-      className="w-3 h-3">
+      className="w-3 h-3"
+    >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -22,10 +24,8 @@ const PrevVerseButton = ({ onClick }) => (
 
 PrevVerseButton.propTypes = {
   onClick: PropTypes.func.isRequired,
-  classes: PropTypes.string.isRequired,
 };
 PrevVerseButton.defaultProps = {
   onClick: () => {},
-  classes: 'w-6 h-6',
 };
 export default PrevVerseButton;
