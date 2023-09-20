3ad9a955c18e6b12a0f0d0851dd1fd21cdd681d6
DenisArger <den.arger@gmail.com>
2023-09-20 22:40:18 +0300

    feat:

--- src/components/LanguageSelect.jsx
+++ src/components/LanguageSelect.jsx
@@ -3,15 +3,24 @@ import { Combobox } from "@headlessui/react";
 import { createBrowserHistory } from "history";
 import { useRecoilState } from "recoil";
 
-import { languageIndexState } from "../atoms";
+import { languageIndexState, translateIndexState } from "../atoms";
 import { filterLangList, langList, searchLanguage } from "../helper";
+import { languageGroups } from "./verseUtils";
+import versesData from "../verses.json";
+
+import NextButton from "./NextButton";
+import PrevButton from "./PrevButton";
 
 function LanguageSelect() {
   let history = createBrowserHistory();
   const [languageIndex, setLanguageIndex] = useRecoilState(languageIndexState);
+  const [translateIndex, setTranslateIndex] =
+    useRecoilState(translateIndexState);
+
   const [selectedLanguage, setSelectedLanguage] = useState({});
   const [query, setQuery] = useState("");
-
+  const currentLanguage = versesData[languageIndex].languageEnglish;
+  const [labelVisible, setLabelVisible] = useState(true);
   useEffect(() => {
     setSelectedLanguage(
       langList.filter((el) => el.index === languageIndex)?.[0]
@@ -25,8 +34,45 @@ function LanguageSelect() {
     }
   }, [history.location.pathname, setLanguageIndex]);
 
+  const goToNextVerse = () => {
+    const languages = Object.keys(languageGroups);
+    const currentIndex = languages.indexOf(currentLanguage);
+    const nextIndex = (currentIndex + 1) % languages.length;
+    const nextLanguage = languages[nextIndex];
+
+    setTranslateIndex(0);
+    setLanguageIndex(
+      versesData.findIndex((verse) => verse.languageEnglish === nextLanguage)
+    );
+    history.push(
+      "/" +
+        nextLanguage +
+        "/" +
+        languageGroups[nextLanguage][0].shortNameTranslate
+    );
+  };
+
+  const goToPrevVerse = () => {
+    const languages = Object.keys(languageGroups);
+    const currentIndex = languages.indexOf(currentLanguage);
+    const prevIndex = (currentIndex - 1 + languages.length) % languages.length;
+    const prevLanguage = languages[prevIndex];
+
+    setTranslateIndex(0);
+    setLanguageIndex(
+      versesData.findIndex((verse) => verse.languageEnglish === prevLanguage)
+    );
+
+    history.push(
+      "/" +
+        prevLanguage +
+        "/" +
+        languageGroups[prevLanguage][0].shortNameTranslate
+    );
+  };
+
   return (
-    <div className="mt-5 mb-9 sm:my-14 lg:my-20 w-full sm:w-72 mx-auto">
+    <div className="mt-5 mb-9 sm:my-14 lg:my-22 w-full sm:w-96 mx-auto">
       <Combobox
         value={selectedLanguage}
         onChange={(newValue) => {
@@ -36,29 +82,57 @@ function LanguageSelect() {
         }}
       >
         <div className="relative">
-          <div className="relative cursor-default overflow-hidden bg-zinc-100 dark:bg-[#1D1F34] p-2 rounded-full flex w-full text-left">
-            <Combobox.Input
-              className="w-full border-none py-2 pl-3 bg-transparent pr-10 text-sm focus:outline-none"
-              displayValue={(lang) => lang.orig}
-              onChange={({ target: { value } }) => setQuery(value)}
-              placeholder="Search Language"
-            />
-            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-11 h-5 cursor-pointer px-3"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                />
-              </svg>
-            </Combobox.Button>
+          <div className="flex items-center gap-x-4">
+            <PrevButton onClick={goToPrevVerse} className={"w-16 h-12"} />
+
+            <div className="relative cursor-default overflow-hidden bg-zinc-100 dark:bg-[#1D1F34] p-2 rounded-full flex w-full text-left">
+              <div className="absolute flex items-baseline gap-x-3">
+                <label
+                  className={`pl-3 text-sm pt-2 ${
+                    labelVisible
+                      ? "opacity-100"
+                      : "opacity-0 pointer-events-none"
+                  } transition-opacity duration-300 flex-shrink-0`}
+                >
+                  {selectedLanguage ? selectedLanguage.orig : ""}
+                </label>
+                <label
+                  className={`text-xs text-gray-500 pt-3 ${
+                    labelVisible
+                      ? "opacity-100"
+                      : "opacity-0 pointer-events-none"
+                  } transition-opacity duration-300 flex-shrink-0`}
+                >
+                  {selectedLanguage ? selectedLanguage.eng : ""}
+                </label>
+              </div>
+
+              <Combobox.Input
+                className="w-full py-2 pl-3 bg-transparent pr-10 text-sm focus:outline-none relative"
+                displayValue={labelVisible ? null : (lang) => lang.orig}
+                onChange={({ target: { value } }) => setQuery(value)}
+                onFocus={() => setLabelVisible(false)}
+                onBlur={() => setLabelVisible(true)}
+              />
+              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-11 h-5 cursor-pointer px-3"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                  />
+                </svg>
+              </Combobox.Button>
+            </div>
+
+            <NextButton onClick={goToNextVerse} className={"w-16 h-12 "} />
           </div>
 
           <Combobox.Options className="absolute mt-4 max-h-60 w-full overflow-auto rounded-3xl bg-zinc-100 dark:bg-[#1D1F34] text-base focus:outline-none shadow-md">
