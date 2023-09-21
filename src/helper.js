87f29049e15c851e3e5ff36474619790ee1f92dd
DenisArger <den.arger@gmail.com>
2023-09-21 11:36:18 +0300

    fix: add check unique lang

--- src/helper.js
+++ src/helper.js
@@ -1,13 +1,24 @@
 import versesData from './verses.json';
 
-export const langList = versesData.map((lang, index) => ({
-  orig: lang.languageOriginal,
-  index,
-  eng: lang.languageEnglish,
-  variants: lang.languageVariants
-    .split(',')
-    .map((el) => el.trim().toLowerCase()),
-}));
+export const langList = versesData.reduce((uniqueLangs, lang, index) => {
+  const langInfo = {
+    orig: lang.languageOriginal,
+    index,
+    eng: lang.languageEnglish,
+    variants: lang.languageVariants
+      .split(',')
+      .map((el) => el.trim().toLowerCase()),
+  };
+
+  const isLangUnique = !uniqueLangs.some((uniqueLang) => uniqueLang.orig === langInfo.orig);
+
+  if (isLangUnique) {
+    uniqueLangs.push(langInfo);
+  }
+
+  return uniqueLangs;
+}, []);
+
 
 export const filterLangList = (filter) => {
   return langList.filter(
