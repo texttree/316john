b64d4db339759cd2e2887992cb5189290c72fca0
Denis Arger <den.arger@gmail.com>
2023-09-26 16:14:36 +0300

    fix: edit logo 3 16

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import DarkModeToggle from "./DarkModeToggle";
 function Header() {
   return (
     <div className="flex flex-row py-6 justify-between items-center relative">
-      <div className="flex-grow font-['Anek_Bangla'] font-bold uppercase text-2xl ml-28 md:ml-96">
-        3:16 John
+      <div className="flex-grow ml-28 md:ml-96">
+        <img src="../../public/logo.png" alt="3:16 John" />
       </div>
       <DarkModeToggle />
       <LocaleSwitch />
